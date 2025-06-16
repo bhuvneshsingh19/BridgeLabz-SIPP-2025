6f92ca366b3c7eb1ee860f4d7665046315862691
Bhuvnesh Singh <bhuvnesh.singh_cs.aiml22@gla.ac.in>
2025-06-16 09:31:50 +0530

    [Bhuvnesh Singh].Add Practice_Problems for Day 01

---
+++ Practice_Problems/hello.java
@@ -0,0 +1,8 @@
+
+//Write a program that prints "Welcome to Bridgelabz!" to the screen.
+
+class hello{
+	public static void main(String[] args){
+	System.out.println("Welcome to Bridgelabz!");
+	}
+}
