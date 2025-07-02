6d2bdf1ffb9dccd17368e59e5f3135688595cbf6
Bhuvnesh Singh <121308807+bhuvneshsingh19@users.noreply.github.com>
2025-07-02 08:49:35 +0530

    Delete Java_Fundamentals directory

--- Java_Fundamentals/Q5.java
+++
@@ -1,8 +0,0 @@
-public class Q5 {
-    public static void main(String[] args) {
-        int fee = 125000, dp = 10;
-        double discount = fee * dp / 100.0;
-        double finalFee = fee - discount;
-        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
-    }
-}
