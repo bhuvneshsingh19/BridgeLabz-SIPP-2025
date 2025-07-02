f4d8b74faec8b6c8f446a816a57f993d7b6ea0c6
Bhuvnesh Singh <121308807+bhuvneshsingh19@users.noreply.github.com>
2025-07-02 08:50:02 +0530

    Delete Practice_Problems directory

--- Practice_Problems/hello.java
+++
@@ -1,8 +0,0 @@
-
-//Write a program that prints "Welcome to Bridgelabz!" to the screen.
-
-class hello{
-	public static void main(String[] args){
-	System.out.println("Welcome to Bridgelabz!");
-	}
-}
